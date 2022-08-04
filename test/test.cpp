2b2769c4bb984a16aaffa9ea178c1983910ee46d
Pema Malling <pemamalling@gmail.com>
2022-08-04 23:55:07 +0200

    Add some test binaries and fix up the test project

--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,35 @@
 #include "hlslcc.h"
 
+#include <fstream>
+#include <string>
+#include <iostream>
+
 int main()
 {
-    //lol, lmao
-    auto* a = &TranslateHLSLFromFile;
-    printf("%p\n", a);
+    unsigned int flags = HLSLCC_FLAG_UNIFORM_BUFFER_OBJECT;
+
+    GlExtensions ext;
+    ext.ARB_explicit_attrib_location = 1;
+    ext.ARB_explicit_uniform_location = 1;
+    ext.ARB_shading_language_420pack = 0;
+    ext.OVR_multiview = 0;
+    ext.EXT_shader_framebuffer_fetch = 0;
+
+    HLSLccSamplerPrecisionInfo info;
+    HLSLccReflection reflection;
+    GLSLShader shader;
+    int ok = TranslateHLSLFromFile("test/a.dxbc", 0, LANG_DEFAULT, &ext, nullptr, info, reflection, &shader);
+
+    if (ok)
+    {
+        std::ofstream out("test/out.hlsl");
+        out << shader.sourceCode;
+        out.close();
+    }
+    else
+    {
+        printf("HLSLcc failed with error code %i\n", ok);
+    }
+
+    return 0;
 }
